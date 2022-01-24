bdf61125a82bbb83e332acbc971a1eedf23e2a0f
Srinu <csrinu236@gmail.com>
2022-01-24 23:02:12 +0530

    okay

--- app.js
+++ app.js
@@ -24,25 +24,29 @@ form.addEventListener("submit", (e) => {
       nValue.value - (nValue.value * vermiValue.value) / 100 - DAPWeight * 0.18;
     let UreaWeight = (remainN / 0.466).toFixed(2);
     insertData.innerHTML = `<p class="answer">
-              With the dosage of N:P2O5:K2O = ${nValue.value}:${pValue.value}:${kValue.value}, in which ${vermiValue.value}% of N is to be
-              supplied through Vermicopost in ${land.value}ha of land, we have to use
+              With the dosage of N:P2O5:K2O = ${nValue.value}:${pValue.value}:${
+      kValue.value
+    }, in which ${vermiValue.value}% of N is to be
+              supplied through Vermicopost in ${
+                land.value
+              }ha of land, we have to use
             </p>
             <div class="ansSheet">
               <article>
                 <p>Vermicompost :</p>
-                <p class="weight">${vermiWeight} kg</p>
+                <p class="weight">${vermiWeight * land.value} kg</p>
               </article>
               <article>
                 <p>DAP (P2O5) :</p>
-                <p class="weight">${DAPWeight} kg</p>
+                <p class="weight">${DAPWeight * land.value} kg</p>
               </article>
               <article>
                 <p>MOP (K2O) :</p>
-                <p class="weight">${MOPWeight} kg</p>
+                <p class="weight">${MOPWeight * land.value} kg</p>
               </article>
               <article>
                 <p>Urea :</p>
-                <p class="weight">${UreaWeight} kg</p>
+                <p class="weight">${UreaWeight * land.value} kg</p>
               </article>
             </div>`;
     displayAlert("Values generated", "success");
